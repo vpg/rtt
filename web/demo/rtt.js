c4dc2408629fbdb6209010f6ba1be9db9fce68d7
Jérôme Bourgeais <bourgeais.jerome@gmail.com>
2014-07-04 10:40:27 +0200

    Adding tracking in demo app

--- web/demo/rtt.js
+++ web/demo/rtt.js
@@ -15,6 +15,9 @@ var Rtt = function(){
 Rtt.prototype.build_page_code= function(){
  return window.location.href;
 }
+Rtt.prototype.build_click_code= function( _dom_element_id){
+ return this.build_page_code() + ':' + _dom_element_id;
+}
 
 /**
  * rtt.view sends a visit:in event type
@@ -69,7 +72,27 @@ Rtt.prototype.leave= function (){
         this.socket.emit('track:out', event_x);
 }
 
-Rtt.prototype.monitor_visit= function( _element_code, _callback){
+Rtt.prototype.click= function ( _dom_element_id){
+    var event_x = { 
+        type: 'click',
+        element_x:  { 
+                 application:   this.app,
+                 protocol:      window.location.protocol,
+                 domain:        window.location.host,
+                 path:          window.location.pathname,
+                 params:        window.location.search,
+                 code:          this.build_click_code( _dom_element_id)
+              },
+        client_x:   {
+                id:             this.client_id,
+                user_agent:     navigator.userAgent,
+              },
+        value: 1
+        };
+    this.socket.emit('track:in', event_x);
+}
+
+Rtt.prototype.monitor= function( _element_code, _callback){
    this.monitors.push( { 
     element_code: _element_code,
     callback: _callback});
@@ -79,13 +102,26 @@ Rtt.prototype.monitor_visit= function( _element_code, _callback){
 var rtt = new Rtt(); 
 
 /// Binding monitoring callbacks
-rtt.socket.on('connect', function () {
-    console.log('connect');
+rtt.socket.on('connect', function (socket) {
+   //socket.broadcast.emit('rtt_ping:'+rtt.build_page_code(), {element_x: { code: rtt.build_page_code()}});
+   //rtt.socket.on('rtt_ping:'+ rtt.build_page_code(), function(){
+   //    console.log('Get ping Send pong %s', 'ping:'+ rtt.build_page_code());
+   //    rtt.socket.emit('rtt_pong',  { element_x : { code : _x.element_x.code}, value : 1});
+   //});
+   //rtt.socket.emit('rtt_ping', {element_x: { code: rtt.build_page_code()}});
+   //rtt.socket.on('rtt_ping:'+ rtt.build_page_code(), function(){
+   //    console.log('Get ping Send pong %s', 'ping:'+ rtt.build_page_code());
+   //    rtt.socket.emit('rtt_pong',  { element_x : { code : _x.element_x.code}, value : 1});
+   //});
     rtt.monitors.forEach( function ( _x) {
-       var c = 'in:'+ _x.element_code;
-       console.log('Monitoring starts for %s', c);
-       rtt.socket.on('in:' + _x.element_code, _x.callback); 
-       rtt.socket.on('out:' + _x.element_code, _x.callback); 
+        var c = 'in:'+ _x.element_code;
+        console.log('Monitoring starts for %s', c);
+        rtt.socket.on('in:' + _x.element_code, _x.callback); 
+        rtt.socket.on('out:' + _x.element_code, _x.callback); 
+       //rtt.socket.on('rtt_pong:' + _x.element_code, function( _pong_x){
+       //    console.log('Get pong %s', _pong_x.element_x.code);
+       //    _x.callback( _pong_x); 
+       //});
     });
 });
     
